ce505f0ffe407bd2151306e7c3566ee17e60e885
astrakk <jackonn223@gmail.com>
2016-10-23 17:31:28 +1000

    Rebuilt the script to cut down char count
    Completely redid the script so it's not as slow and bulky. Looks a
    little cleaner now too.
    
    Still no comments though. Next update.

--- net_value.js
+++ net_value.js
@@ -1,37 +1,35 @@
 function (context, args) {// from:"", to:""
 	if(!args || !args.from || !args.to){
-		return "Please remember to include !from! and !to! keys."	
+		return "Please remember to include !from! and !to! keys.";
 	}
-	var lib = #s.scripts.lib();
+	
+	var i = 0, spent = 0, earn = 0;
 	var tr = #s.accts.transactions({count:"all"});
-	var i, time, val, valgc;
-	var memo;
-	var spent = 0, earn = 0, net = 0;
+	var lib = #s.scripts.lib();
 	var log = [];
-	log.push("Range from: " + args.from + "\nRange to: " + args.to + "\n")
-	tr.reverse(i => i+1)
+	tr.reverse(i => i+1);
 	for(i in tr){
-		memo = "   ";
-		time = lib.to_game_timestr(tr[i].time);
-		val = tr[i].amount;
-		valgc = lib.to_gc_str(val);
+		var time = lib.to_game_timestr(tr[i].time);
+		var val = tr[i].amount;
+		var gc = lib.to_gc_str(tr[i].amount);
+
+		if(tr[i].memo){
+			var memo = "`1[M]` ";
+		} else {
+			var memo = "    ";
+		}
+
 		if(time > args.from && time <= args.to){
 			if(tr[i].sender == context.caller){
-				if(tr[i].memo){memo = "`1[M]`";}
-				spent = spent+val;
-				net = net-val;
-				log.push(memo + " `5" + time + "` `1-" + valgc + "`");
-			} else if(tr[i].sender != context.caller){
-				if(tr[i].memo){memo = "`1[M]`";}
-				earn = spent+val;
-				net = net+val;
-				log.push(memo + " `2" + time + "` `1+" + valgc + "`");
+				spent = spent + val;
+				log.push(memo + "`5" + time + "` `1-" + gc + "`");
+			} else {
+				log.push(memo + "`2" + time + "` `1+" + gc + "`");
+				earn = earn + val;
 			}
 		}
+			
 	}
-	spent = lib.to_gc_str(spent)
-	earn = lib.to_gc_str(earn)
-	net = lib.to_gc_str(net)
-	log.push("\n`5Spent total:` " + spent + "\n`2Earned total:` " + earn + "\n!Net! !total!: " + net + " ")
+	log.push("\n`2Earned:` " + lib.to_gc_str(earn) + "\n`5Spent:`  " + lib.to_gc_str(spent) +"\n!Net!:    " + lib.to_gc_str(earn-spent) + " ");
 	return log;
 };
